022181f05d3e1c13cad9fcae7cb817ea547d375f
hangulo <hector@loggly.com>
2015-06-01 22:51:22 -0700

    Embedded PinBoard into AJax/Angular Router.  ::Note:: Still defining the controller within the partial... not best practice?

--- static/js/main.js
+++ static/js/main.js
@@ -6,11 +6,15 @@ app.config(function($routeProvider) {
             .when('/',
             {
                 templateUrl: 'Partials/index.html'
-                //controller: 'HomeController' //Why doesnt like this?
+                //controller: 'HomeController' //Why doesnt like this? have to put in html?!
             })
             .when('/about',
             {
                 templateUrl: 'Partials/about.html'
+            })
+            .when('/pin',
+            {
+                templateUrl: 'Partials/pin.html'
             })
             .otherwise(
             {
@@ -26,4 +30,40 @@ app.controller('HomeController', function($scope){
         'Blog post 2',
         'Blog post 3'
     ]
+});
+
+app.controller("AppCtrl", function($http) {
+    var app = this;
+
+    $http.get("/api/pin").success(function(data)
+    {
+        app.pins = data.objects;
+
+    });
+
+    app.addPin= function () {
+        $http.post("api/pin", {"title":"new", "image":"http://placekitten.com/200/200/?image="+ app.pins.length})
+            .success(function(data){
+                app.pins.push(data);
+            })
+    };
+
+    app.addPinRandom= function () {
+        $http.post("api/pin", {"title":"new", "image":"http://randomimage.setgetgo.com/get.php"})
+            .success(function(data){
+                app.pins.push(data);
+            })
+    };
+
+    app.deletePin = function(pin) {
+        $http.delete("/api/pin/"+ pin.id).success(function(response){
+            app.pins.splice(app.pins.indexOf(pin), 1);
+        })
+    };
+
+    app.updatePin = function(pin){
+        $http.put("/api/pin/"+ pin.id,pin);
+
+    }
+
 });
